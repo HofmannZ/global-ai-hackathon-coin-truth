0b8811a031e585dd8e6d8e6dcbbda68874b2e621
Zino Hofmann <zino@hofmann.amsterdam>
2017-06-24 18:52:51 +0200

    🕸 Crawling the whole board

--- src/crawler/input_data/spiders/bitcointalk.py
+++ src/crawler/input_data/spiders/bitcointalk.py
@@ -6,12 +6,33 @@ class BitcointalkSpider(scrapy.Spider):
     name = 'bitcointalk'
     allowed_domains = ['bitcointalk.org']
     start_urls = [
-        # 'https://bitcointalk.org/index.php?board=1.0',
-        'https://bitcointalk.org/index.php?topic=1972053.0',
-        'https://bitcointalk.org/index.php?topic=1784753.0',
+        'https://bitcointalk.org/index.php?board=1.0',
     ]
 
+    firstBoardPage = True
+    firstTopicPage = True
+
     def parse(self, response):
+        topics = response.css('div.tborder table.bordercolor')[-1]
+
+        self.firstTopicPage = True
+
+        if topics.css('table span a::attr(href)') is not None:
+            for link in topics.css('span a::attr(href)'):
+                url = link.extract()
+                yield scrapy.Request(url, callback=self.parseTopic)
+
+        link = response.css('td#toppages span.prevnext a::attr(href)')
+
+        if (len(link) > 1) or self.firstBoardPage:
+            url = link[0 if self.firstBoardPage else 1].extract()
+
+            if self.firstBoardPage:
+                self.firstBoardPage = False
+
+            yield scrapy.Request(url, callback=self.parse)
+
+    def parseTopic(self, response):
         for post in response.css('form#quickModForm tr'):
 
             yield {
@@ -21,3 +42,13 @@ class BitcointalkSpider(scrapy.Spider):
                 'date': post.css('td.td_headerandpost div.smalltext::text').extract_first(),
                 'text': post.css('div.post::text').extract(),
             }
+
+        link = response.css('td.middletext span.prevnext a::attr(href)')
+
+        if (len(link) > 1) or self.firstTopicPage:
+            url = link[0 if self.firstTopicPage else 1].extract()
+
+            if self.firstTopicPage:
+                self.firstTopicPage = False
+
+            yield scrapy.Request(url, callback=self.parseTopic)
